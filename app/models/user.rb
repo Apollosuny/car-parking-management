39a1cd058b0fe0085932f40277fdbd585ccdd29c
Apollo <baotrung06092003@gmail.com>
2024-05-09 22:09:09 +0700

    fix: initial profile when create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,21 @@ class User < ApplicationRecord
   has_many :vehicle
   has_many :booking
 
+  # validate data
+  validates :username, presence: true, uniqueness: true
+
+  after_create :init_profile
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Helper func
+  private 
+
+  def init_profile
+    Profile.create(user: self)
+  end
+
 end
